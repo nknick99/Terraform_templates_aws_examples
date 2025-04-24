f26d8491b59279f159d8217cc1287b531e42f5e0
nknick99 <kudachenikhil@gmail.com>
2025-04-24 23:33:23 +0530

    updated s3 variables

--- s3/variables.tf
+++ s3/variables.tf
@@ -1,20 +1,18 @@
 variable "aws_region" {
     description = "AWS region"
     type = string
-}
-
-variable "aws_profile" {
-    description = "AWS CLI Profile"
-    type = string
+    default = "ua-east-1"
 }
 
 
 variable "tag_name" {
     description = "Tag Name"
     type = string
+    default = "dev"
 }
 
 variable "tag_env" {
     description = "Tag Environment"
     type = string
+    default = "test_env"
 }   
